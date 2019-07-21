76533df04c1cf26ba9ba356486709ec8cc649455
atmka <corp.dvd@gmail.com>
2019-07-21 23:06:29 +0400

    Коммит для того, чтобы потом на работе запулить

--- src/service/DateService.java
+++ src/service/DateService.java
@@ -4,29 +4,36 @@ import entity.ExportDateName;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 
 public class DateService {
-    public Long differenceBirthdayDateAndNowDate (String birthdayDate, ExportDateName exportDateName) {
-        Date nowDate = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    public int differenceBirthdayDateAndNowDate (String birthdayDate, ExportDateName exportDateName) {
+        DateTimeFormatter pattern = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthdayDatePars;
         try {
-            switch (exportDateName.getName()) {
-                case "секунды":
-                        return new Long((nowDate.getTime() - dateFormat.parse(birthdayDate).getTime()) / 1000);
-                case "минуты":
-                        return new Long((nowDate.getTime() - dateFormat.parse(birthdayDate).getTime()) / (1000 * 60));
-                case "часы":
-                        return new Long((nowDate.getTime() - dateFormat.parse(birthdayDate).getTime()) / (1000 * 60 * 60));
-                case "дни":
-                        return new Long((nowDate.getTime() - dateFormat.parse(birthdayDate).getTime()) / (1000 * 60 * 60 * 24));
-                case "месяцы":
-                    return new Long((nowDate.getTime() - dateFormat.parse(birthdayDate).getTime()) / (1000 * 60 * 60 * 24*100));
-                    default:
-                        return null;
-            }
-        }catch (ParseException e) {
-            return null;
+            birthdayDatePars = LocalDate.parse(birthdayDate,pattern);
+        }
+        catch (DateTimeParseException e){
+            birthdayDatePars = LocalDate.now();
+        }
+        Period period = birthdayDatePars.until(LocalDate.now());
+        switch (exportDateName.getName()){
+            case "секунды":
+                return period.getDays() * 24 * 60 * 60;
+            case "минуты":
+                return period.getDays() * 24 * 60;
+            case "часы":
+                return period.getDays() * 24;
+            case "дни":
+                return period.getDays();
+            case "месяцы":
+                return period.getMonths();
+            default:
+                return 0;
         }
     }
 }
